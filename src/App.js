27f1651081e51f891e8b287c342b4ebeb6197543
Akshat Mittal <akshatmittal.0007@gmail.com>
2023-01-05 22:31:32 +0530

    'making basic structue with complete maps'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import "./App.css";
-import WorldMap from "./components/MapSelector";
-import SimpleMaps from "./components/SimpleMaps/SimpleMaps";
+import WorldMap from "./components/LeafLet/MapSelector";
+import SimpleMaps from "./components/MapSection/SimpleMaps";
+import { Grid } from "@mui/material";
+import NavMenu from "./components/NavMenu/NavMenu";
+import Intro from "./components/Introduction/Intro";
+import MapForm from "./components/MapSection/MapForm";
 
 function App() {
   return (
     <>
-      {/* <WorldMap /> */}
-      <SimpleMaps />
+      <div>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <div
+              style={{
+                backgroundColor: "green",
+                height: "120px",
+              }}
+            >
+              Build your own zero-emission electricity supply
+            </div>
+          </Grid>
+          <Grid item xs={12} md={3} lg={2}>
+            <div
+              style={{
+                backgroundColor: "blue",
+              }}
+            >
+              <NavMenu />
+            </div>
+          </Grid>
+          <Grid item xs={12} md={9} lg={10}>
+            <div
+              style={{
+                backgroundColor: "white",
+              }}
+            >
+              <Intro />
+              <MapForm />
+            </div>
+          </Grid>
+        </Grid>
+      </div>
     </>
   );
 }
